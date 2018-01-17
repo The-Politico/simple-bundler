5a99654af3cc01243755edee468468bbca09d3d8
Tyler Fisher <tfisher@politico.com>
2018-01-17 14:21:53 -0500

    no gdocs, no react

--- src/js/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import HelloMessage from './components/HelloMessage';
-
-require('../scss/app.scss');
-
-const mountNode = document.getElementById('app');
-render(<HelloMessage name="Tyler" />, mountNode);
